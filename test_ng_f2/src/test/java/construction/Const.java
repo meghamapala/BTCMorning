9dc49262f3492a548a0b4b20ca09fd2c97c75d3a
meghamapala@gmail.com <meghamapala@gmail.com>
2023-01-13 08:41:54 -0500

    first commit

---
+++ test_ng_f2/src/test/java/construction/Const.java
@@ -0,0 +1,39 @@
+package construction;
+
+public class Const {
+	
+	String d;
+	String b;
+	int l;
+	
+	Const(){
+		
+	}
+	
+    Const(String d){
+    	this.d=d;
+		
+	}
+    
+    int add(int l, int c) {
+    	
+    	int T=l+c;
+    	
+    	return T;
+    	
+    }
+    
+    Const(String d, String b){
+    	this.d=d;
+    	this.b=b;
+		
+	}
+    
+    
+	
+	
+
+}
+
+
+
